ebedb56137d055fb83ff90efe64a5840f84c8fff
giualbuq <giulia.albsilva@gmail.com>
2024-10-23 16:04:46 -0300

    Desafio Heroi

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hello world")
+let nome = "Homem de Ferro";
+let XP = 1500;
+let experiencia;
+
+if (XP < 1000) {
+    experiencia = "Ferro";
+} else if (XP > 1000 && XP <= 2000){
+    experiencia = "Bronze";
+} else if (XP > 2000 && XP <= 5000){
+    experiencia = "Prata";
+} else if (XP > 5000 && XP <= 7000){
+    experiencia = "Ouro";
+} else if (XP > 7000 && XP <= 8000){
+    experiencia = "Platina";
+} else if (XP > 8000 && XP <= 9000){
+    experiencia = "Ascendente";
+} else if (XP > 9000 && XP <= 10000){
+    experiencia ="Imortal";
+} else {
+    experiencia = "Radiante";
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + experiencia);
